700ffd2c7161800a84c46ebbf5eeebfb2792b9b8
Guaru34 <19170095@uttcampus.edu.mx>
2025-12-03 02:48:58 -0600

    cambio minimo

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,2 +1,2 @@
-export { AuthContext } from "./auth-context"
-export { AuthProvider } from "./auth-provider"
+export { AuthContext } from "./auth-context";
+export { AuthProvider } from "./auth-provider";
